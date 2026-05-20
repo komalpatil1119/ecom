e5fdb1c3550efddb06c7cbb3c6ad17b3b5a9037c
Komal Patil <patil.komal1119@gmail.com>
2026-05-20 18:16:10 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,98 @@
+// STICKY HEADER
+
+const header = document.getElementById("mainHeader");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 80) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+
+});
+// MOBILE MENU
+
+const mobileToggle = document.getElementById("mobileToggle");
+const mainMenu = document.getElementById("mainMenu");
+
+mobileToggle.addEventListener("click", () => {
+
+    mainMenu.classList.toggle("active");
+
+    if (mainMenu.classList.contains("active")) {
+
+        mobileToggle.innerHTML =
+            '<i class="bi bi-x-lg"></i>';
+
+        document.body.style.overflow = "hidden";
+
+    } else {
+
+        mobileToggle.innerHTML =
+            '<i class="bi bi-list"></i>';
+
+        document.body.style.overflow = "auto";
+    }
+
+});
+
+
+// MOBILE DROPDOWN
+
+const shopToggle = document.querySelector(".shop-toggle");
+const hasDropdown = document.querySelector(".has-dropdown");
+
+shopToggle.addEventListener("click", function (e) {
+
+    // ONLY MOBILE
+
+    if (window.innerWidth < 1200) {
+
+        e.preventDefault();
+
+        hasDropdown.classList.toggle("open");
+
+    }
+
+});
+
+
+//home tab bundle
+document.addEventListener("DOMContentLoaded", () => {
+
+    const buttons = document.querySelectorAll(".filter-btn");
+    const cards = document.querySelectorAll(".bundle-card");
+
+    buttons.forEach(btn => {
+
+        btn.addEventListener("click", () => {
+
+            buttons.forEach(b => b.classList.remove("active"));
+            btn.classList.add("active");
+
+            const filter = btn.dataset.filter;
+
+            cards.forEach(card => {
+
+                if (filter === "all") {
+
+                    card.style.display = "block";
+
+                } else if (card.classList.contains(filter)) {
+
+                    card.style.display = "block";
+
+                } else {
+
+                    card.style.display = "none";
+
+                }
+
+            });
+
+        });
+
+    });
+
+});
